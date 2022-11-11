66153db993f5eb70ee39431aa538f64dcf9ead5b
KinjiKawaguchi <kawakin0310@icloud.com>
2022-11-12 02:21:36 +0900

    Set CurrentView

---
+++ ModernCSVCombiner/ModernCSVCombiner/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModernCSVCombiner.Core;
+
+namespace ModernCSVCombiner.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public InputFilesViewModel InputVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            InputVM = new InputFilesViewModel();
+            CurrentView = InputVM;
+        }
+    }
+}
